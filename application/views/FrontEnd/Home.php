b67c76cd4aa1a053e17456904efbf75c9325fd7c
daniel <2015030282@upsin.edu.mx>
2018-03-30 00:31:35 -0700

    avances home front 80%

--- application/views/FrontEnd/Home.php
+++ application/views/FrontEnd/Home.php
@@ -43,8 +43,11 @@
 
 <div class="container">
   <section class="row">
+    <div class="col-md-5 col-md-offset-1"><h3 class="titleFoodMenu">Food menu</h3></div>
+    <div class="col-md-5"><h3>Today Special</h3></div>
+  </section>
+  <section class="row">
     <div class="container">
-      <h3 class="titleFoodMenu">Food menu</h3>
       <div class="col-md-5 col-md-offset-1 lista">
         <ol>
           <?php foreach (array(1,2,3,4,5,6) as &$key) {  ?>
@@ -57,186 +60,118 @@
       </div>
       <div class="col-md-5">
         <div class="left-c cuadro">
-          <br>
+          <img src="<?= base_url('assets/sources/img/galeria2.jpg'); ?>" alt="">
+          <p>texto<br>$345</p>
         </div>
         <div class="right-c cuadro">
-          <br>
+          <img src="<?= base_url('assets/sources/img/defaultRest.jpg'); ?>" alt="">
+          <p>texto<br>$345</p>
         </div>
       </div>
     </div>
   </section>
-
-  <section class="row" style="background: blue">
-    <br>
-  </section>
-
-  <section class="row" style="background: green">
-    <br>
-  </section>
-</div>
-		<!--
-
-	<div id="dos_imagenes"  class="container" >
-		<div  class="col-md-8" >
-    
-        
-        <div  style="top:10%;left:80%;position:absolute;" class="col-md-8">
-         <h2>Today Special</h2>           
-         <img  src="<?= base_url('assets/sources/img/comi1.jpg'); ?>" class="img-thumbnail col-md-8" width="600" height="600"> 
-         <img style="top:16%;left:68%;position:absolute;" src="../../assets/sources/img/comi2.jpg" class="img-thumbnail col-md-8"  width="600" height="600"> 
-          
-        <div class="col-md-8">
-          <h2></h2>
-          <div  style="font:yellow;font-size:15;"  class="well well-sm">Pizza Marfuera $89.00</div>
-          
-       </div>
-       <div class="col-md-4">
-          <h2></h2>
-          <div  style="font:yellow;font-size:15;width:330px;height:40px;"  class="well well-sm">Pizza Marfuera $89.00</div>
-          
-       </div>
-</div>
-
-     </div>
-
-	<div id="listaMenu" class="col-md-8">
-  <div  class="list-group">
-
-     <div><h2 style="left:1px;" >Food Menu</h2></div>
-    
-    <a style="width:500px;height:80px;"href="#" class="list-group-item col-md-4">
-      <h4 class="list-group-item-heading">ultimate organnic fruit    $50</h4>
-      <p class="list-group-item-text">List Group Item Text</p>
-    </a>
-    <a style="width:500px;height:80px;" href="#" class="list-group-item col-md-4">
-      <h4 class="list-group-item-heading">Second List Group Item Heading</h4>
-      <p class="list-group-item-text">List Group Item Text</p>
-    </a>
-    <a  style="width:500px;height:80px;"href="#" class="list-group-item col-md-4">
-      <h4 class="list-group-item-heading">Third List Group Item Heading</h4>
-      <p class="list-group-item-text">List Group Item Text</p>
-    </a>
-    <a style="width:500px;height:80px;" href="#" class="list-group-item col-md-4">
-      <h4 class="list-group-item-heading">Second List Group Item Heading</h4>
-      <p class="list-group-item-text">List Group Item Text</p>
-    </a>
-    <a style="width:500px;height:80px;" href="#" class="list-group-item col-md-4">
-      <h4 class="list-group-item-heading">Second List Group Item Heading</h4>
-      <p class="list-group-item-text">List Group Item Text</p>
-    </a>
-    <div class="btn-group btn-group-lg col-md-8">
-    <button style="top:50px;left:2%;position:absolute; background:orange;font-size: 20px;padding:14px 58px;" type="button" 
-    class="btn btn-primary">Export Food Menu</button>
-  </div>
-  </div>
-	</div>
-
-
-
-<div>
-	<div class="container" id="Reserve">
-    
-    <img style="width:2110px;height:600px;top:1300px;left:1px;position:absolute;" src="../../assets/sources/img/fondo5.jpg" class="fondo"  width="1970" height="800">
-<div class="col-md-3 col-md-offset-4">
-
-    <div style="margin-left:-200%;height:366px;top:1%;top:300px;" class="col-md-4 col-md-offset-4"> 
-        
-         <div class="container"><h2  style="left:1px;top:30%;color:white;" >Reserve your table</h2></div>
-<div></div>
-    <div  class="form-group">
-      <input style="width:600px;height:50px;" id="email" type="text" class="form-control" name="email" placeholder="Name">
-    </div>
-    <div  style="color:white;" class="form-group">
-      <input style="width:600px;height:50px;" id="email" type="text" class="form-control" name="email" placeholder="Time">
-    </div>
-    <div  style="color:white;" class="form-group">
-      <input style="width:600px;height:50px;" id="email" type="text" class="form-control" name="email" placeholder="Guest">
-    </div>
-    </div>
-    </div>
-
-        <div  style="margin-left:44.333333%"class="col-md-4 col-md-offset-4">
-
-    <div  class="form-group">
-      <input style="width:600px;height:50px;top:-200px;" id="email" type="text" class="form-control" name="email" placeholder="Email Address">
-    </div>
-    <div  style="color:white;" class="form-group">
-      <input style="width:600px;height:50px;" id="email" type="text" class="form-control" name="email" placeholder="Date">
-    </div>
-    <div  style="color:white;" class="form-group">
-      <input style="width:600px;height:50px;" id="email" type="text" class="form-control" name="email" placeholder="phone">
-
-    </div>
-<div style="top: 1px;left: -68%;width:200px;position:relative;"> <img   src="../../assets/sources/img/numero.png"></div>
+  <section class="row">
     <div class="container">
-
-  <button  style="top:214px;left:-140%;position:absolute; background:orange;font-size: 20px;padding:14px 58px;"type="button" class="btn btn-primary">Make Reservation</button>
-  
-  </div>
-  </div>
-  </div>
-
-
-<div  id="happyclient_imagen"class="media">
-  <div class="media-left media-top">
-    <img style="width: 453px;height: 395px;top: 2019px;left: 67px;position: absolute;border-radius: 50px;" src="../../assets/sources/img/cena.jpeg" class="media-object">
-    <img style="width: 827px;height: 271px;top: 1820px;left: 575px;position: absolute;" src="../../assets/sources/img/happy.png" class="media-object">
-  </div> 
-  </div>
-<div id="happyclient_texto" style="width: 827px;height: 271px;top: 2000px;left: 575px;position: absolute;" class="container">
-  <a href="#demo" class="btn btn-info" data-toggle="collapse"> Read More</a>
-  <div style="font-size: 30px;font-weight:cursive;font-family:sans-serif;" id="demo" class="collapse">
-   " Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-  </div>
-  <h2 style="font-weight:BOLD;">DR.PRABHKARAN JHON</h2>
-</div>
- 
+      <div class="col-md-5 col-md-offset-1">
+        <button class="btn explore">Explor food menu</button>
+      </div>
+    </div>
+  </section>
 </div>
 
 
 
 
-
-
-<div id="galeria">
-
-
-<div style="width:100%;height:399px;top: 621px;left: -392px;" id="myCarousel" class="carousel slide col-md-4" data-ride="carousel">
-  <ol class="carousel-indicators"> 
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li> 
-     <li data-target="#myCarousel" data-slide-to="1" class="active"></li> 
-     <li data-target="#myCarousel" data-slide-to="2"class="active"></li> 
-  </ol>
-
-  <div style="width:2000px;height:430px;background-color:gray;" class="carousel-inner">
-    <h1 style="left:200px;" class="container">Our Galley</h1> 
-    <div class="item active">
-      <img style="width:571px;" class="container"src="../../assets/sources/img/galeria1.jpg" alt="">
+<div class="reserve">
+  <h2 class="text-center">Reserve your table</h2>
+  <div class="container" >
+    <div class="row form-group inp">
+      <div class="col-md-4 col-md-offset-2">
+        <input type="text" class="form-control" placeholder="Name*">
+      </div>
+      <div class="col-md-4">
+        <input type="text" class="form-control" placeholder="Email address*">
+      </div>
     </div>
-
-    <div  style="width:2000px;height:600px;" class="item">
-      <img style="width:571px;" class="container" src="../../assets/sources/img/galeria2.jpg" alt="">
+    <div class="row form-group inp">
+      <div class="col-md-4 col-md-offset-2">
+        <input type="text" class="form-control" placeholder="Time*">
+      </div>
+      <div class="col-md-4">
+        <input type="text" class="form-control" placeholder="Date*">
+      </div>
     </div>
-
-    <div  style="width:2000px;height:600px;" class="item">
-      <img style="width:571px;" class="container" src="../../assets/sources/img/fondoo.jpg" alt="">
+    <div class="row form-group inp">
+      <div class="col-md-4 col-md-offset-2">
+        <input type="text" class="form-control" placeholder="Guests*">
+      </div>
+      <div class="col-md-4">
+        <input type="text" class="form-control" placeholder="Phone*">
+      </div>
+    </div>
+    <div class="row form-group">
+      <div class="col-md-2 col-md-offset-2"><br>
+        <button class="btn explore">Make reservation</button>
+      </div>
+      <div class="col-md-6 callP">
+        <div class="call col-xs-4"><p>You can also call<br>for a reservation</p></div>
+        <div class="col-xs-6 text-left"><p>1-007 000 005</p></div>
+      </div>
     </div>
   </div>
-
-   <a class="left carousel-control" href="#myCarousel" data-slide="prev"> 
-    <span class="glyphicon glyphicon-chevron-left"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a style="left: 1800px;"class="right carousel-control" href="#myCarousel" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-    <span class="sr-only">Next</span>
-  </a>
-        
-
-</div>
 </div>
 
+<div class="container">
+  <section class="row happy">
+    <h2 class="text-center">Happy Clients</h2>
+    <div class="col-xs-10 col-xs-offset-1 carousel slide vertical" id="myCarouselV">
+      <ol class="carousel-indicators indicatorCar">
+        <li data-target="#myCarouselV" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarouselV" data-slide-to="1" class="active"></li>
+        <li data-target="#myCarouselV" data-slide-to="2" class="active"></li>
+      </ol>
+      <div class="carousel-inner contenidoCarV">
+      <div class="active item">
+        <div class="col-md-3" >
+          <img src="<?= base_url('assets/sources/img/galeria2.jpg'); ?>" alt="">
+        </div>
+        <div class="col-md-9" >
+          <br>bbb
+        </div>
+      </div>
+      <div class="item">
+        <div class="col-md-3" >
+          <img src="<?= base_url('assets/sources/img/defaultRest.jpg'); ?>" alt="">
+        </div>
+        <div class="col-md-9" >
+          <br>d
+        </div>
+      </div>
+      <div class="item">
+        <div class="col-md-3" >
+          <img src="<?= base_url('assets/sources/img/galeria1.jpg'); ?>" alt="">
+        </div>
+        <div class="col-md-9" >
+          <br>oo
+        </div>
+      </div>
+    </div>
+
+<a class="left carousel-control carCtrl" href="#myCarouselV" data-slide="prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
+    <a class="right carousel-control carCtrl" href="#myCarouselV" data-slide="next"><i class="glyphicon glyphicon-chevron-right"></i></a>
+      
+    </div>
+  </section>
+</div>
+
+<div class="ourCooks">
+  <br>
+</div>
 
 <?php $this->load->view('FrontEnd/Global/Footer'); ?>
+
+<script>
+  $('.carousel').carousel({
+  interval: 3000
+});
+</script>
