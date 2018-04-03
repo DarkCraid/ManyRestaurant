2e805135088af8d3cdf475208c0aa81871f4468d
daniel <2015030282@upsin.edu.mx>
2018-04-03 01:31:03 -0600

    avances admin about us

--- application/models/Restaurant/AboutUsModel.php
+++ application/models/Restaurant/AboutUsModel.php
@@ -19,23 +19,18 @@ class AboutUsModel extends CI_Model {
 
 	public function check(){
 
-		$query = $this->db->query('SELECT * FROM nuestrahistoria WHERE EXISTS (SELECT id FROM nuestrahistoria WHERE id = 1)')->row();
-		//print_r($query);
+		$query = $this->db->query('SELECT * FROM datos_restaurante WHERE EXISTS (SELECT id FROM datos_restaurante WHERE id = 1)')->row();
 		return $query;
 	}
 
-	public function insert($data){
-		print_r($data['id']);
+	public function SetAboutUs($data){
+		$this->db->set($data);
+		$this->db->where('id',1);
+		$this->db->update('datos_restaurante');
+	}
 
-		if ($data['id'] == 1 ){
-			$this->db->set('titulo',$data['titulo']);
-			$this->db->set('contenido',$data['contenido']);
-			$this->db->set('foto',$data['foto']);
-			$this->db->where('id','1');
-			$this->db->update('nuestrahistoria');
-		}else{
-			$this->db->insert('nuestrahistoria',$data);
-		}
+	public function GetPhotos(){
+		
 	}
 
 }
--- application/views/Administrador/NosotrosActualizar.php
+++ application/views/Administrador/NosotrosActualizar.php
@@ -2,29 +2,110 @@
 <?php $this->load->view('Administrador/Global/AsideLeft');?>
 
 <div class="content-wrapper">
-	<section class="content-header">
-		<h1>Actualizar ventana Sobre Nosotros / About us</h1>
-	</section>
+    <section class="content-header">
+      	<section class="content">
+	        <div class="row box">
+    			<div class="container">
+    				<div class="col-md-2"><h3>About Us</h3></div>
+    				<button class="btn btn-success pull-right" id="save" style="position: relative; bottom: -10px;">SAVE</button>
+    			</div>
+	        </div>
+			
+	        <div class="row box">
+	        	<div class="container" id="contentenido">
+    				<div class="row">
+    					<div class="col-md-12">
+    						<form enctype="multipart/form-data" role="form" method="post" action="<?= base_url('index.php/Admin/NosotrosActualizar/do_upload'); ?>"  id="addBlogForm">
+    								<br>
+    								<div class="row form-group">
+					                    <div class="col-md-2 text-right">
+					                        <label class="col-form-label mr-2 control-label">Address: </label>
+					                    </div>
+					                    <div class="col-md-10">
+					                        <input maxlength="100" type="text" name="adrres" class="form-control" placeholder="*" value="<?= $key->address; ?>" required>
+					                    </div>
+					                </div>
+					                <div class="row form-group">
+					                    <div class="col-md-2 text-right">
+					                        <label class="col-form-label mr-2 control-label">Email: </label>
+					                    </div>
+					                    <div class="col-md-4">
+					                        <input maxlength="100" type="email" name="email" class="form-control" placeholder="*" value="<?= $key->email; ?>" required>
+					                    </div>
+					                </div>
+					                <div class="row form-group">
+					                    <div class="col-md-2 text-right">
+					                        <label class="col-form-label mr-2 control-label">Phone: </label>
+					                    </div>
+					                    <div class="col-md-2">
+					                        <input maxlength="10" type="text" name="phone" class="form-control" placeholder="*" value="<?= $key->telefono; ?>" required>
+					                    </div>
+					                </div>
+					                <div class="row form-group" style="background-color: #e8e8e8;">
+					                	<h4>Monday to Friday</h4>
+					                </div>
+					                <div class="row form-group">
+					                    <div class="col-md-2 text-right">
+					                        <label class="col-form-label mr-2 control-label">From: </label>
+					                    </div>
+					                    <div class="col-md-2">
+					                        <input maxlength="10" type="time" name="hi_lv" class="form-control" placeholder="*" value="<?= $key->hi_lv; ?>" required>
+					                    </div>
+					                    <div class="col-md-1 text-center">
+					                        <label class="col-form-label mr-2 control-label">To: </label>
+					                    </div>
+					                    <div class="col-md-2">
+					                        <input maxlength="10" type="time" name="hf_lv" class="form-control" placeholder="*" value="<?= $key->hf_lv; ?>" required>
+					                    </div>
+					                </div>
+					                <div class="row form-group" style="background-color: #e8e8e8;">
+					                	<h4>Saturday to Sunday</h4>
+					                </div>
+					                <div class="row form-group">
+					                    <div class="col-md-2 text-right">
+					                        <label class="col-form-label mr-2 control-label">From: </label>
+					                    </div>
+					                    <div class="col-md-2">
+					                        <input maxlength="10" type="time" name="hi_sd" class="form-control" placeholder="*" value="<?= $key->hi_sd; ?>" required>
+					                    </div>
+					                    <div class="col-md-1 text-center">
+					                        <label class="col-form-label mr-2 control-label">To: </label>
+					                    </div>
+					                    <div class="col-md-2">
+					                        <input maxlength="10" type="time" name="hf_sd" class="form-control" placeholder="*" value="<?= $key->hf_sd; ?>" required>
+					                    </div>
+					                </div>
+					                <div class="row form-group">
+					                    <div class="col-md-2 text-right">
+					                        <label class="col-form-label mr-2 control-label">Our History: </label>
+					                    </div>
+					                    <div class="col-md-10">
+					                        <textarea maxlength="100" type="text" name="history" class="form-control" placeholder="*" required style="height: 120px;"><?= $key->historia; ?></textarea><br>
+					                    </div>
+					                </div>
+					                <div class="row form-group">
+					                    <h3 class="text-center">Photo</h3>
+					                    <div class="col-md-offset-2 col-md-2">
+					                    	<label>Choese other photo</label>
+					                    </div>
+					                    <div class="col-md-3">
+					                    	<select name="photo" id="photo" class="form-control">
+					                    		<option value="1">asd</option>
+					                    	</select>
+					                    </div>
+					                </div>
+					                <div class="col-md-8 col-md-offset-2">
+				                        <img src="<?= base_url('assets/sources/img/restaurant/'.$historia[0]->foto);?>" alt="fotoHistoria" style="width: 100%;">
+				                    </div>
+    						</form>
+    					</div>
 
-	<form id="formActuaNosotros" action="<?php echo base_url('index.php/Admin/NosotrosActualizar/do_upload') ?>" method="POST" enctype="multipart/form-data">
-		<div class="form-group has-feedback">
-			<input type="text" class="form-control" placeholder="Titulo" name="titulo" value="<?php echo $key->titulo ?>">
-			<span class="glyphicon glyphicon-pencil form-control-feedback"></span>
-		</div>
-		<div class="form-group">
-			<textarea placeholder="Contenido" name="contenido" class="form-control"><?php echo $key->contenido ?>
-			</textarea>
-		</div>
-		<div class="form-group has-feedback">
-			<img src="<?php echo base_url('assets/sources/img/'.$key->foto);?>">
-			<input type="file" class="form-control" name="userFile" value="<?php echo $key->foto ?>">
-		</div>
-		<input type="hidden" name="id" value="<?php echo $key->id ?>">
-		<div class="form-group">
-			<input type="submit">
-		</div>
-	</form>
-</div>
+    				</div>
+    			</div>
+			</div>
+		</section>
+    </section>
+  </div>
 
 <?php $this->load->view('Administrador/Global/AsideRight');?>
 <?php $this->load->view('Administrador/Global/Footer');?>
